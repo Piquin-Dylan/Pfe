0316142e89378249daec52835b69574fb768e2db
Piquin-Dylan <dylan.piquin@student.hepl.be>
2026-03-28 19:45:25 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,22 @@ Route::get('/inscription', function () {
 Route::get('/login', function () {
     return view('client.login');
 })->name('login');
-Route::get('/create', function () {
-    return view('client.create_team');
-})->middleware('auth');
-Route::get('/join', function () {
-    return view('client.join_team');
-});
-Route::get('/profile', function () {
-    return view('client.form_profile');
-})->middleware('auth');
-Route::get('/hub', function () {
-    return view('client.hub');
-})->middleware('auth');
+
 Route::get('/logout', function () {
     return view('client.hub');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/create', function () {
+        return view('client.create_team');
+    });
+    Route::get('/join', function () {
+        return view('client.join_team');
+    });
+    Route::get('/profile', function () {
+        return view('client.form_profile');
+    });
+    Route::get('/hub', function () {
+        return view('client.hub');
+    });
 });
